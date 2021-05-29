9222ffd0f7b101cc58cc872dde1c22dc404b62ea
Yingjie Ling <yingjie.ling@tufts.edu>
2021-05-29 15:42:13 -0400

    commit

---
+++ Hacker News/Views/WebView.swift
@@ -0,0 +1,30 @@
+//
+//  WebView.swift
+//  Hacker News
+//
+//  Created by Ling Yingjie on 5/29/21.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
